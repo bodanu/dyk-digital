70c4654da7f8986163c7a98c3c552b42be712528
Bogdan <bogdan.hiriscau@gmail.com>
2021-12-23 13:01:46 +0200

    better sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,5 +1,5 @@
 import React from "react";
-import fs from "fs";
+// import fs from "fs";
 import instance from "./../components/utils/Interceptor";
 
 const Sitemap = () => {};
@@ -9,44 +9,20 @@ export const getServerSideProps = async ({ res }) => {
     development: "http://localhost:3000",
     production: "https://dyk.digital",
   }[process.env.NODE_ENV];
-  const staticPages = fs
-    .readdirSync("pages")
-    .filter((staticPage) => {
-      return ![
-        "_app.js",
-        "_document.js",
-        "_error.js",
-        "sitemap.xml.js",
-      ].includes(staticPage);
-    })
-    .map((staticPagePath) => {
-        staticPagePath = staticPagePath.replace(".js", "");
-      return `${baseUrl}/${staticPagePath}`;
-    });
+
 
   const request = await instance.get(process.env.REACT_APP_API_URL + '/api/posts');
   const documents = request.data || [];
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${staticPages
-        .map((url) => {
-          return `
-            <url>
-              <loc>${url}</loc>
-              <lastmod>${new Date().toISOString()}</lastmod>
-              <changefreq>monthly</changefreq>
-              <priority>1.0</priority>
-            </url>
-          `;
-        })
-        .join("")}
+
       ${documents
         .map((post) => {
           return `
               <url>
                 <loc>${baseUrl}/posts/${post.id}</loc>
                 <lastmod>${post.created_at}</lastmod>
-                <changefreq>monthly</changefreq>
+                <changefreq>daily</changefreq>
                 <priority>1.0</priority>
               </url>
             `;
