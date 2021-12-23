fbef8071e1f9c9427dedd408fc0519d2587b7a83
Bogdan <bogdan.hiriscau@gmail.com>
2021-12-23 12:53:58 +0200

    better sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,67 @@
+import React from "react";
+import fs from "fs";
+import instance from "./../components/utils/Interceptor";
+
+const Sitemap = () => {};
+
+export const getServerSideProps = async ({ res }) => {
+  const baseUrl = {
+    development: "http://localhost:3000",
+    production: "https://dyk.digital",
+  }[process.env.NODE_ENV];
+  const staticPages = fs
+    .readdirSync("pages")
+    .filter((staticPage) => {
+      return ![
+        "_app.js",
+        "_document.js",
+        "_error.js",
+        "sitemap.xml.js",
+      ].includes(staticPage);
+    })
+    .map((staticPagePath) => {
+        staticPagePath = staticPagePath.replace(".js", "");
+      return `${baseUrl}/${staticPagePath}`;
+    });
+
+  const request = await instance.get(process.env.REACT_APP_API_URL + '/api/posts');
+  const documents = request.data || [];
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${staticPages
+        .map((url) => {
+          return `
+            <url>
+              <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+        })
+        .join("")}
+      ${documents
+        .map((post) => {
+          return `
+              <url>
+                <loc>${baseUrl}/posts/${post.id}</loc>
+                <lastmod>${post.created_at}</lastmod>
+                <changefreq>monthly</changefreq>
+                <priority>1.0</priority>
+              </url>
+            `;
+        })
+        .join("")}
+    </urlset>
+  `;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
